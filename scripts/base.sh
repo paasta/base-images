781d4387e9481d9f1031bda1bc7880a40130af14
Jonas Pfenniger <zimbatm@zimbatm.com>
2013-10-16 22:46:02 +0100

    Make sure all images have the same sources

--- scripts/base.sh
+++ scripts/base.sh
@@ -11,3 +11,14 @@ cat <<EOF > /etc/apt/apt.conf.d/02compress-indexes
 Acquire::GzipIndexes "true";
 Acquire::CompressionTypes::Order:: "gz";
 EOF
+
+# Configure the source
+cat <<APT_SOURCE > /etc/apt/sources.list
+deb http://us.archive.ubuntu.com/ubuntu/ precise main restricted universe multiverse
+deb http://us.archive.ubuntu.com/ubuntu/ precise-updates main restricted universe multiverse
+deb http://us.archive.ubuntu.com/ubuntu/ precise-backports main restricted universe multiverse
+deb http://security.ubuntu.com/ubuntu precise-security main restricted universe multiverse
+APT_SOURCE
+
+apt-get update -qy
+
