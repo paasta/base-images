dde9f796a8371b65265adfe67306c8bf2d0ca884
Jonas Pfenniger <zimbatm@zimbatm.com>
2013-10-16 22:00:18 +0100

    Install chef and berkshelf in a chef-prepare.sh script

--- scripts/chef-prepare.sh
+++ scripts/chef-prepare.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+set -e
 
+# Install chef
 apt-get install -y curl
+curl -L https://www.opscode.com/chef/install.sh | bash -s -- -v 11.6.0
+
+# Install berkshelf because it takes a long time to do so
+apt-get install -y build-essential
+/opt/chef/embedded/bin/gem install berkshelf --no-ri --no-rdoc
+ln -sf /opt/chef/embedded/bin/berks /usr/local/bin/berks
