74357973e9280973e7faa8a37a8645d843117644
Aaron McNeal <aaron@mediacore.com>
2014-05-15 14:16:43 -0700

    Install autoconf in script

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -11,6 +11,7 @@ export DEBIAN_FRONTEND='noninteractive'
 # Without libdbus virtualbox would not start automatically after compile
 apt-get install -qy --no-install-recommends libdbus-1-3
 apt-get install -qy dkms linux-headers-$(uname -r)
+apt-get install -qy autoconf
 
 # Installing the virtualbox guest additions
 mount -o loop VBoxGuestAdditions.iso /mnt
