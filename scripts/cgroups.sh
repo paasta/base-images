fd2337fcb0e865ae7d43e01a9dbceb3a5aef0da8
Vivien Schilis <vivien.schilis@gmail.com>
2015-11-04 08:58:41 +0100

    hvm instance for ubuntu

--- scripts/cgroups.sh
+++ scripts/cgroups.sh
@@ -1,2 +1,10 @@
+#!/bin/sh
+set -e
+
+if [ "$PACKER_BUILDER_TYPE" != "virtualbox-iso" ] && [ "$PACKER_BUILDER_TYPE" != "vmware" ]; then
+  echo "Skipping, $PACKER_BUILDER_TYPE is not our target"
+  exit 0
+fi
+
 perl -p -i -e 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/g'  /etc/default/grub
 /usr/sbin/update-grub
